9a368e31e9ed50b24f9ed64c8ad65b64f964e72e
Simo Naatula <git@naatu.la>
2020-12-11 15:39:11 +0200

    Fix cross origin for api

--- routes/apis/summaryApi.js
+++ routes/apis/summaryApi.js
@@ -1,13 +1,13 @@
 import * as reportService from '../../services/reportService.js'
 
-// Public
-
 const lastWeek = async({response}) => {
+  response.headers.set('Access-Control-Allow-Origin', '*')
   response.body = await reportService.getSummaryBetween(Date.now() - 604800000, Date.now())
   if(Object.keys(response.body).length === 0){ response.status = 400 }
 }
 
 const day = async({response, params}) => {
+  response.headers.set('Access-Control-Allow-Origin', '*')
   const date = `${params.year}-${params.month}-${params.day}`
   response.body = await reportService.getDaySummary(date)
   if(Object.keys(response.body).length === 0){ response.status = 400 }
