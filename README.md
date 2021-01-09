ee6e22709e030f774db62d0b52d80908ce6beae6
Simo Naatula <git@naatu.la>
2021-01-09 02:39:52 +0200

    Improve database structure

--- README.md
+++ README.md
@@ -20,30 +20,22 @@ CREATE TABLE users (
   password CHAR(60) NOT NULL
 );
 
-CREATE TABLE morning_reports (
+CREATE TABLE reports (
   id SERIAL PRIMARY KEY,
   user_id INT NOT NULL REFERENCES users(id),
   date DATE NOT NULL,
-  sleep_duration NUMERIC(6,2) NOT NULL,
-  sleep_quality SMALLINT NOT NULL,
-  mood SMALLINT NOT NULL
-);
-
-CREATE TABLE evening_reports (
-  id SERIAL PRIMARY KEY,
-  user_id INT NOT NULL REFERENCES users(id),
-  date DATE NOT NULL,
-  exercise NUMERIC(6,2) NOT NULL,
-  studying NUMERIC(6,2) NOT NULL,
-  eating SMALLINT NOT NULL,
-  mood SMALLINT NOT NULL
+  type SMALLINT NOT NULL,
+  sleep_duration NUMERIC(6,2),
+  sleep_quality SMALLINT,
+  mood SMALLINT,
+  exercise NUMERIC(6,2),
+  studying NUMERIC(6,2),
+  eating SMALLINT
 );
 
 CREATE INDEX users_email_index ON users (email);
 
-CREATE INDEX morning_reports_date_user_id_index ON morning_reports (date, user_id);
-
-CREATE INDEX evening_reports_date_user_id_index ON evening_reports (date, user_id);
+CREATE INDEX reports_date_user_id_index ON reports (date, user_id);
 ```
 
 ## Running
--- services/reportService.js
+++ services/reportService.js
@@ -1,14 +1,14 @@
 import { executeQuery } from "../database/database.js";
 
 const addMorning = async(user_id, date, sleep_duration, sleep_quality, mood) => {
-  await executeQuery('DELETE FROM morning_reports WHERE user_id = $1 AND date = $2', user_id, date)
-  await executeQuery('INSERT INTO morning_reports (user_id, date, sleep_duration, sleep_quality, mood) VALUES ($1, $2, $3, $4, $5)', user_id, date, sleep_duration, sleep_quality, mood)
+  await executeQuery('DELETE FROM reports WHERE user_id = $1 AND date = $2 AND type = 1', user_id, date)
+  await executeQuery('INSERT INTO reports (user_id, date, sleep_duration, sleep_quality, mood, type) VALUES ($1, $2, $3, $4, $5, 1)', user_id, date, sleep_duration, sleep_quality, mood)
   return;
 }
 
 const addEvening = async(user_id, date, exercise, studying, eating, mood) => {
-  await executeQuery('DELETE FROM evening_reports WHERE user_id = $1 AND date = $2', user_id, date)
-  await executeQuery('INSERT INTO evening_reports (user_id, date, exercise, studying, eating, mood) VALUES ($1, $2, $3, $4, $5, $6)', user_id, date, exercise, studying, eating, mood)
+  await executeQuery('DELETE FROM reports WHERE user_id = $1 AND date = $2 AND type = 2', user_id, date)
+  await executeQuery('INSERT INTO reports (user_id, date, exercise, studying, eating, mood, type) VALUES ($1, $2, $3, $4, $5, $6, 2)', user_id, date, exercise, studying, eating, mood)
   return;
 }
 
@@ -20,8 +20,8 @@ const find = async(user_id, date) => {
   date = date.toISOString().slice(0,10)
 
   const report = {}
-  const m = await executeQuery('SELECT * FROM morning_reports WHERE user_id = $1 AND date = $2 LIMIT 1', user_id, date)
-  const e = await executeQuery('SELECT * FROM evening_reports WHERE user_id = $1 AND date = $2 LIMIT 1', user_id, date)
+  const m = await executeQuery('SELECT * FROM reports WHERE user_id = $1 AND date = $2 AND type = 1 LIMIT 1', user_id, date)
+  const e = await executeQuery('SELECT * FROM reports WHERE user_id = $1 AND date = $2 AND type = 2 LIMIT 1', user_id, date)
   const morning = m.rowsOfObjects()
   const evening = e.rowsOfObjects()
   if(morning.length > 0){
@@ -42,26 +42,11 @@ const getDaySummary = async(date, user_id = null) => {
 
   let data;
   if(user_id) {
-    data = {
-      date: date,
-      sleep_duration: (await executeQuery('SELECT AVG(sleep_duration) FROM morning_reports WHERE date = $1 AND user_id = $2', date, user_id)).rows[0]?.[0],
-      sleep_quality: (await executeQuery('SELECT AVG(sleep_quality) FROM morning_reports WHERE date = $1 AND user_id = $2', date, user_id)).rows[0]?.[0],
-      exercise: (await executeQuery('SELECT AVG(exercise) FROM evening_reports WHERE date = $1 AND user_id = $2', date, user_id)).rows[0]?.[0],
-      studying: (await executeQuery('SELECT AVG(studying) FROM evening_reports WHERE date = $1 AND user_id = $2', date, user_id)).rows[0]?.[0],
-      eating: (await executeQuery('SELECT AVG(eating) FROM evening_reports WHERE date = $1 AND user_id = $2', date, user_id)).rows[0]?.[0],
-      mood: await getAvgMood(date, user_id)
-    }
+    data = (await executeQuery('SELECT AVG(sleep_duration) AS sleep_duration, AVG(sleep_quality) AS sleep_quality, AVG(exercise) AS exercise, AVG(studying) AS studying, AVG(eating) AS eating, AVG(mood) AS mood FROM reports WHERE date = $1 AND user_id = $2', date, user_id)).rowsOfObjects()[0]
   } else {
-    data = {
-      date: date,
-      sleep_duration: (await executeQuery('SELECT AVG(sleep_duration) FROM morning_reports WHERE date = $1', date)).rows[0]?.[0],
-      sleep_quality: (await executeQuery('SELECT AVG(sleep_quality) FROM morning_reports WHERE date = $1', date)).rows[0]?.[0],
-      exercise: (await executeQuery('SELECT AVG(exercise) FROM evening_reports WHERE date = $1', date)).rows[0]?.[0],
-      studying: (await executeQuery('SELECT AVG(studying) FROM evening_reports WHERE date = $1', date)).rows[0]?.[0],
-      eating: (await executeQuery('SELECT AVG(eating) FROM evening_reports WHERE date = $1', date)).rows[0]?.[0],
-      mood: await getAvgMood(date)
-    }
+    data = (await executeQuery('SELECT AVG(sleep_duration) AS sleep_duration, AVG(sleep_quality) AS sleep_quality, AVG(exercise) AS exercise, AVG(studying) AS studying, AVG(eating) AS eating, AVG(mood) AS mood FROM reports WHERE date = $1', date)).rowsOfObjects()[0]
   }
+  data.date = date
 
   return data;
 }
@@ -77,26 +62,11 @@ const getSummaryBetween = async(firstDay, lastDay, user_id = null) => {
 
   let data;
   if(user_id) {
-    data = {
-      dates: {from: firstDay, to: lastDay},
-      sleep_duration: (await executeQuery('SELECT AVG(sleep_duration) FROM morning_reports WHERE date BETWEEN $1 AND $2 AND user_id = $3', firstDay, lastDay, user_id)).rows[0]?.[0],
-      sleep_quality: (await executeQuery('SELECT AVG(sleep_quality) FROM morning_reports WHERE date BETWEEN $1 AND $2 AND user_id = $3', firstDay, lastDay, user_id)).rows[0]?.[0],
-      exercise: (await executeQuery('SELECT AVG(exercise) FROM evening_reports WHERE date BETWEEN $1 AND $2 AND user_id = $3', firstDay, lastDay, user_id)).rows[0]?.[0],
-      studying: (await executeQuery('SELECT AVG(studying) FROM evening_reports WHERE date BETWEEN $1 AND $2 AND user_id = $3', firstDay, lastDay, user_id)).rows[0]?.[0],
-      eating: (await executeQuery('SELECT AVG(eating) FROM evening_reports WHERE date BETWEEN $1 AND $2 AND user_id = $3', firstDay, lastDay, user_id)).rows[0]?.[0],
-      mood: (await executeQuery('SELECT AVG(mood) FROM (SELECT mood FROM morning_reports WHERE DATE BETWEEN $1 AND $2 AND user_id = $3 UNION ALL SELECT mood FROM evening_reports WHERE DATE BETWEEN $1 AND $2 AND user_id = $3) AS t', firstDay, lastDay, user_id)).rows[0]?.[0]
-    }
+    data = (await executeQuery('SELECT AVG(sleep_duration) AS sleep_duration, AVG(sleep_quality) AS sleep_quality, AVG(exercise) AS exercise, AVG(studying) AS studying, AVG(eating) AS eating, AVG(mood) AS mood FROM reports WHERE date BETWEEN $1 AND $2 AND user_id = $3', firstDay, lastDay, user_id)).rowsOfObjects()[0]
   } else {
-    data = {
-      dates: {from: firstDay, to: lastDay},
-      sleep_duration: (await executeQuery('SELECT AVG(sleep_duration) FROM morning_reports WHERE date BETWEEN $1 AND $2', firstDay, lastDay)).rows[0]?.[0],
-      sleep_quality: (await executeQuery('SELECT AVG(sleep_quality) FROM morning_reports WHERE date BETWEEN $1 AND $2', firstDay, lastDay)).rows[0]?.[0],
-      exercise: (await executeQuery('SELECT AVG(exercise) FROM evening_reports WHERE date BETWEEN $1 AND $2', firstDay, lastDay)).rows[0]?.[0],
-      studying: (await executeQuery('SELECT AVG(studying) FROM evening_reports WHERE date BETWEEN $1 AND $2', firstDay, lastDay)).rows[0]?.[0],
-      eating: (await executeQuery('SELECT AVG(eating) FROM evening_reports WHERE date BETWEEN $1 AND $2', firstDay, lastDay)).rows[0]?.[0],
-      mood: (await executeQuery('SELECT AVG(mood) FROM (SELECT mood FROM morning_reports WHERE DATE BETWEEN $1 AND $2 UNION ALL SELECT mood FROM evening_reports WHERE DATE BETWEEN $1 AND $2) AS t', firstDay, lastDay)).rows[0]?.[0]
-    }
+    data = (await executeQuery('SELECT AVG(sleep_duration) AS sleep_duration, AVG(sleep_quality) AS sleep_quality, AVG(exercise) AS exercise, AVG(studying) AS studying, AVG(eating) AS eating, AVG(mood) AS mood FROM reports WHERE date BETWEEN $1 AND $2', firstDay, lastDay)).rowsOfObjects()[0]
   }
+  data.dates = {from: firstDay, to: lastDay}
 
   return data;
 }
@@ -105,9 +75,9 @@ const getAvgMood = async(date, user_id = null) => {
   date = new Date(date).toISOString().slice(0,10)
   var r;
   if(user_id){
-    r = await executeQuery('SELECT AVG(mood) FROM (SELECT mood FROM morning_reports WHERE DATE = $1 AND user_id = $2 UNION ALL SELECT mood FROM evening_reports WHERE DATE = $1 AND user_id = $2) AS t', date, user_id)
+    r = await executeQuery('SELECT AVG(mood) FROM reports WHERE DATE = $1 AND user_id = $2', date, user_id)
   } else {
-    r = await executeQuery('SELECT AVG(mood) FROM (SELECT mood FROM morning_reports WHERE DATE = $1 UNION ALL SELECT mood FROM evening_reports WHERE DATE = $1) AS t', date)
+    r = await executeQuery('SELECT AVG(mood) FROM reports WHERE DATE = $1', date)
   }
   return r.rows[0]?.[0];
 }
